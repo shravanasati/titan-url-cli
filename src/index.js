e68b5f9e1034238ca39c6915a26de4753afa9dcc
Shravan Asati <dev.shravan@protonmail.com>
2021-07-05 22:47:30 +0530

    👽️ made cli compatible with the new api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 const inquirer = require("inquirer")
 const http = require("http")
 
-const shorten = (url) => {
+const shorten = (url, aliasType, slug) => {
 	const options = {
-		hostname: "http://titan-url.herokuapp.com",
+		hostname: "localhost",
+		port: 5000,
 		path: "/shorten",
-		method: "get",
-		data: url
+		method: "GET",
+		headers: {
+			"original-url": url,
+			"alias-type": aliasType,
+			"slug": slug
+		}
 	}
-	const req = http.request(options, res => {
-		res.on('data', d => {
-			console.log(`Here's your shortened URL ${d["message"]}`);
+
+	const req = http.get(options, res => {
+		res.on('data', resp => {
+			let jsonResp = JSON.parse(resp)
+			let ok = jsonResp["ok"]
+			let message = jsonResp["message"]
+
+			if (!ok) {
+				console.log(`\x1b[31m An error occured: ${message} \x1b[0m`) // * red color for failure
+			}
+			else {
+				console.log(`\x1b[32m Here's your shortened URL: ${message} \x1b[0m`) // * green color for success
+			}
 		})
 	})
 
@@ -23,17 +38,23 @@ const shorten = (url) => {
 
 inquirer
 	.prompt([
-		{ "type": "input", "name": "url", "message": "Enter the URL you want to shorten:" }
+		{ "type": "input", "name": "url", "message": "Enter the URL you want to shorten:" },
+		{ "type": "list", "name": "aliasType", "message": "Choose the alias type:", "choices": ["random", "custom"] }
 	])
 	.then(answers => {
 		let url = answers["url"]
-		const valid = /^(ftp|http|https):\/\/[^ "]+$/.test(url);
+		const aliasType = answers["aliasType"]
 
-		if (!valid) {
-			console.log("The URL you've entered is wrong!");
-		}
+		if (aliasType == "random") {
+			const slug = ""
+			shorten(url, aliasType, slug)
 
-		else {
-			shorten(url)
+		} else {
+			inquirer
+				.prompt([{ "type": "input", "name": "slug", "message": "Enter the custom slug you want for the shortned URL:" }])
+				.then(slug_answers => {
+					const slug = slug_answers["slug"]
+					shorten(url, aliasType, slug)
+				})
 		}
 	})
