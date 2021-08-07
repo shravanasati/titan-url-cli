76477333cfe10427c3085f65a576513de30d2fdf
Shravan Asati <dev.shravan@protonmail.com>
2021-08-07 23:58:15 +0530

    🐛 fix parsing bug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+// imports
 const inquirer = require("inquirer")
-const http = require("http")
+const request = require("request")
 
 // colors
 const RED = "\x1b[31m"
@@ -10,41 +11,36 @@ const RESET = "\x1b[0m"
 const GREEN_CHECK_MARK = "\u2705"
 const RED_CROSS_MARK = "\u274C"
 
+// shorten function which makes a request to the titan-url api
 const shorten = (url, aliasType, slug) => {
-	const options = {
-		hostname: "localhost",
-		port: 5000,
-		path: "/shorten",
-		method: "GET",
-		headers: {
-			"original-url": url,
-			"alias-type": aliasType,
-			"slug": slug
-		}
+	let data = {
+		"original-url": url,
+		"alias-type": aliasType,
+		"slug": slug
 	}
 
-	const req = http.get(options, res => {
-		res.on('data', resp => {
-			let jsonResp = JSON.parse(resp)
-			let ok = jsonResp["ok"]
-			let message = jsonResp["message"]
-
-			if (!ok) {
-				console.log(`${RED_CROSS_MARK} ${RED}An error occured: ${message}${RESET}`) // * red color for failure
-			}
-			else {
-				console.log(`${GREEN_CHECK_MARK} ${GREEN}Here's your shortened URL: ${message}${RESET}`) // * green color for success
-			}
-		})
+	request.post({
+		url: "http://titan-url.herokuapp.com/shorten",
+		headers: {
+			'User-Agent': "titan-url-cli",
+			"Content-Type": "application/json"
+		},
+		json: data
+	}, function (error, response, body) {
+		let ok = body["ok"]
+		let message = body["message"]
+	
+		if (!ok) {
+			console.log(`${RED_CROSS_MARK} ${RED}An error occurred: ${message}${RESET}`) // * red color for failure
+		}
+		else {
+			console.log(`${GREEN_CHECK_MARK} ${GREEN}Here's your shortened URL: ${message}${RESET}`) // * green color for success
+		}
 	})
 
-	req.on('error', error => {
-		console.error(`An error occured: ${error}`)
-	})
-
-	req.end()
 }
 
+// main TUI
 inquirer
 	.prompt([
 		{ "type": "input", "name": "url", "message": "Enter the URL you want to shorten:" },
@@ -60,7 +56,7 @@ inquirer
 
 		} else {
 			inquirer
-				.prompt([{ "type": "input", "name": "slug", "message": "Enter the custom slug you want for the shortned URL:" }])
+				.prompt([{ "type": "input", "name": "slug", "message": "Enter the custom slug you want for the shortened URL:" }])
 				.then(slug_answers => {
 					const slug = slug_answers["slug"]
 					shorten(url, aliasType, slug)
